2d4547f7cf461931a848a7123a498bc879ea26cf
Shadowf6 <iansh5787@gmail.com>
2025-03-28 19:32:09 -0500

    11

--- Assets/Scripts/SaveData.cs
+++ Assets/Scripts/SaveData.cs
@@ -2,40 +2,73 @@ using System.IO;
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public static class GameData // remove "static" tag later (when i actually bother to code the save data)
+public static class GameData
 {
     public static int playerHP;
-    public static int souls;
+    public static int coins;
     public static List<Soul> playerDeck = new List<Soul>();
+}
+
+[System.Serializable]
+public class SerializableData
+{
+    public int playerHP;
+    public int coins;
+    public List<Soul> playerDeck;
+
+    public SerializableData()
+    {
+        playerHP = GameData.playerHP;
+        coins = GameData.coins;
+        playerDeck = GameData.playerDeck;
+    }
+
+    public void Update()
+    {
+        GameData.playerHP = playerHP;
+        GameData.coins = coins;
+        GameData.playerDeck = playerDeck;
+    }
 }
 
 public class SaveData : MonoBehaviour
 {
     string filePath;
 
-    public SaveData()
+    void Start()
     {
         filePath = Application.persistentDataPath + "/lantern" + SessionData.selectedSave + ".json";
     }
 
     void NewDataFile()
     {
+        // add defaults
 
+        GameData.coins = 0;
+        GameData.playerDeck = new List<Soul>();
+
+
+        SaveGame();
+    } 
+
+    public void SaveGame()
+    {
+        SerializableData data = new SerializableData();
+
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(filePath, json);
     }
 
-    void SaveGame()
+    public void LoadGame()
     {
-
-    }
-
-    void LoadGame()
-    {
-
-    }
-
-    void GetField(string name)
-    {
-
+        if (File.Exists(filePath))
+        {
+            string json = File.ReadAllText(filePath);
+            SerializableData data = JsonUtility.FromJson<SerializableData>(json);
+            data.Update();
+        } else
+        {
+            NewDataFile();
+        }
     }
 }
